3fb767e30d405d0e90c598423c7f45a5761350f8
benoit delville <benoit.delville@coretex.com>
2020-04-17 17:13:18 +1200

    Also parse plurals

--- android/resource-to-csv.py
+++ android/resource-to-csv.py
@@ -1,20 +1,45 @@
 import xml.dom.minidom
 from xml.dom.minidom import Node
 
-filename = "./strings.xml"
-oputputfilename = "./output.csv"
+fileName = 'strings'
+projectName = 'my-module'
+
+filename = "path/to/project/" + projectName + "/src/main/res/values/" + fileName + ".xml"
+oputputfilename = "path/to/output/output-" + projectName + "-" + fileName + ".csv"
 fileout = open(oputputfilename, "w")
 
+
+def nodeDataToString(node):
+	text = node.firstChild.data
+	text = text.replace("\n", "\\n")
+	return text
+    # return text.encode('utf8') // If required
+
+def readItemsArray(node):
+	line = ""
+	for item in node.getElementsByTagName("item"):
+		try:
+			if item.hasAttribute("quantity"):
+				line = line + "quantity=" + item.getAttribute("quantity") + ";"
+			line = line + nodeDataToString(item) + ";"
+		except Exception:
+			line = line + "error-script;"
+			pass
+	return line
+
+################
+# Start SCRIPT #
+################
 doc = xml.dom.minidom.parse(filename)
 
 for node in doc.getElementsByTagName("string"):
 	name = node.getAttribute("name")
-	line = name+";"
+	line =  "string;" + name + ";"
 	
 	try:
-		line = line+node.firstChild.data
+		line = line + nodeDataToString(node)
 	except Exception:
-		line = line+" "
+		line = line + " "
 		pass
 	line = line + "\n"
 	fileout.write(line)
@@ -22,16 +47,13 @@ for node in doc.getElementsByTagName("string"):
 	
 for node in doc.getElementsByTagName("string-array"):
 	name = node.getAttribute("name")
-	line = name
-	
-	for item in node.getElementsByTagName("item"):
-		line = line + ";"
-		try:
-			line = line+item.firstChild.data
-		except Exception:
-			line = line+" "
-			pass
-	line = line + "\n"
+	line = "string-array;" + name + ";" + readItemsArray(node) + "\n"
 	fileout.write(line)
+
 	
-fileout.close();
+for node in doc.getElementsByTagName("plurals"):
+	name = node.getAttribute("name")
+	line = "plurals;" + name + ";" + readItemsArray(node) + "\n"
+	fileout.write(line)
+
+fileout.close()
